2bb866f12922e619760d815092ed3f997c2adf7d
umuttolga <umuttolgawow@gmail.com>
2023-03-05 16:34:02 -0800

    hope init

--- screens/PostIdea.js
+++ screens/PostIdea.js
@@ -4,22 +4,8 @@ import logo from "../assets/logo.png";
 import UserInput from "../components/reusable compinents/UserInput";
 import { useForm } from "react-hook-form";
 import MyButton from "../components/reusable compinents/MyButton";
-import { Dropdown } from "react-native-material-dropdown-v2-fixed";
-import {SelectDropdown, DropdownData} from "expo-select-dropdown";
 const PostIdea = () => {
-  const [selected, setSelected] = useState<DropdownData<string, string> | null>(null);  
-  const [data] = useState<DropdownData<string, string>[]>([  
-       {key: "1", value: "Toothbrush"}, 
-       {key: "2", value: "Laptop"}, 
-       {key: "3", value: "Sunglasses"},  
-     {key: "4", value: "Baseball"}, 
-     {key: "5", value: "Scissors"}, 
-     {key: "6", value: "Bicycle"},  
-     {key: "7", value: "Camera"}, 
-     {key: "8", value: "Umbrella"}, 
-     {key: "9", value: "Backpack"},  
-     {key: "10", value: "Water bottle"}  
-  ]);
+  const {control, }
   return (
     <ScrollView>
       <View className="m-auto bg-[#1A849B] h-[100vh] w-screen items-center">
@@ -49,17 +35,7 @@ const PostIdea = () => {
         </View>
         <View className="mr-auto flex-row items-center  mt-4 p-4">
           <Text className="text-xl">Category:</Text>
-          <View className=" ml-2  w-[70%]">
-            <Dropdown
-            
-            
-              icon="chevron-down"
-              iconColor="#E1E1E1"
-              label="Select a category"
-              data={DATA}
-              
-            />
-          </View>
+          <View className=" ml-2  w-[70%]"></View>
         </View>
         <MyButton
           buttonStyle="bg-[#6578DC] mt-4 p-4 px-10 rounded-xl"
